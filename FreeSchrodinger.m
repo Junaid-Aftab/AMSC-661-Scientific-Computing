6f8dfac597985ef61a3f6c952ea12be8caae81c5
MJunaidAftab <50157884+MJunaidAftab@users.noreply.github.com>
2023-05-17 16:49:36 -0400

    Add files via upload

--- FreeSchrodinger.m
+++ FreeSchrodinger.m
@@ -5,9 +5,9 @@ fsz = 15; % fontsize
 % solves u_t = i/2 u_{xx};
 
 %Space Parameters
-dx = 0.01;
 L = 40;                 %Length of x-interval
-N = round(L/dx);        %Coallocation Points
+N = 4096;               %Coallocation Points
+dx = L/N;
 x = linspace(-L/2,L/2,N+1);
 x(end) = [];
 
@@ -37,6 +37,19 @@ figure(1); clf;   % Create a new figure
 hold on;
 legendLabels = cell(1, 6);  % Initialize cell array to store legend labels
 
+%Plot initial figures
+%figure(1); clf; 
+%probability = prob(sigma,k0,x,t);
+%hpic1 = plot(x, sqrt(probability),'LineWidth',2,'color','b'); 
+%hold on;
+%hpic2 = plot(x, abs(u0),'LineWidth',2,'color','r'); 
+%hpic3 = plot(x, abs(u0),'LineWidth',2,'color','g'); 
+%xlabel('x','FontSize',fsz);
+%ylabel('Norm Squared of Wave Function','FontSize',fsz);
+%set(gca,'FontSize',fsz);
+%grid
+%drawnow
+
 while(t <= tmax+epsilon)
 
     %METHOD 1: Discrete Fourier Transform
@@ -59,28 +72,37 @@ while(t <= tmax+epsilon)
     k4=rhs(dx,u_rk+dt*k3);
     unew_rk=u_rk+(dt/6).*(k1+2*k2+2*k3+k4);
 
-    %update plots
-    if abs(5*t/tmax-round(5*t/tmax)) < 10*epsilon
-        plot(x, abs(u_dft),'LineWidth',2);
-        hold on;
-        legendLabels{int32(round(5*t/tmax))+1} = sprintf('Time = %g', t);
-        xlabel('x','FontSize',fsz);
-        ylabel('Norm Squared of Wave Function','FontSize',fsz);
-        set(gca,'FontSize',fsz);
-        grid on;
-        drawnow
-    end
-    %disp(legendLabels)
-
     % Update varibales etc.
     probability = prob(sigma,k0,x,t);
     trapz(x,probability);
     u_dft = unew_dft;
     u_rk = unew_rk;
+
+    %update plots
+    z = abs(5*t/tmax-round(5*t/tmax));
+    if z < 10*epsilon
+        plot(x, abs(u_dft),'LineWidth',2);
+        %plot(x, sqrt(probability),'LineWidth',2);
+        legendLabels{int32(round(5*t/tmax))+1} = sprintf('Time = %g', t);
+        xlabel('x','FontSize',fsz);
+        ylabel('Norm Squared of Wave Function','FontSize',fsz);
+        title('Numerical Solution');
+        %xlim([-L/4,L/4])
+        set(gca,'FontSize',fsz);
+        set(gcf, 'PaperSize', [6.25 7.5]);
+        set(gcf, 'PaperPositionMode', 'manual');
+        set(gcf, 'PaperPosition', [0 0 6.25 7.5]);
+        xlim([-L/4,L/4]);
+        grid on;
+        drawnow
+    end
+
+    %update time
     t=t+dt;
 
     %set(hpic1,'xdata',x,'ydata', sqrt(probability) );
     %set(hpic2,'xdata',x,'ydata', abs(u_dft) );
+    %set(hpic3,'xdata',x,'ydata', abs(u_rk) );
     
     pause(pause_duration);
 end
@@ -101,16 +123,3 @@ function RHS=rhs(dx,u_inter)
     shiftedLeft = circshift(u_inter, [0, -1]);
     shiftedRight = circshift(u_inter, [0, +1]);
     RHS = (1i/(2*dx^2)).*( shiftedLeft + shiftedRight - 2.*u_inter );
-
-
-%Plot initial figures
-%figure(1); clf; 
-%probability = prob(sigma,k0,x,t);
-%hpic1 = plot(x, sqrt(probability),'LineWidth',2,'color','b'); 
-%hold on;
-%hpic2 = plot(x, abs(u0),'LineWidth',2,'color','r'); 
-%xlabel('x','FontSize',fsz);
-%ylabel('Norm Squared of Wave Function','FontSize',fsz);
-%set(gca,'FontSize',fsz);
-%grid
-%drawnow
